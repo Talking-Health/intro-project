fe73679ac9db36fd40a2f7515a9077297c209db3
STERIN SAJI <91521416+Sterin32Saji@users.noreply.github.com>
2025-10-11 23:35:15 +0100

    text formatting changes

--- public/js/dom.js
+++ public/js/dom.js
@@ -1,35 +1,34 @@
-
-
-
-
 /**
- * 
- * @param { object } element 
- * @param { string } className 
- * @returns 
+ *
+ * @param { object } element
+ * @param { string } className
+ * @returns
  */
-export function findancestorbyclass( element, className ) {
+export function findancestorbyclass(element, className) {
+  let currentElement = element;
 
-  let currentElement = element
-
-  while( currentElement && !currentElement.classList.contains( className ) ) {
-    currentElement = currentElement.parentNode
+  while (currentElement && !currentElement.classList.contains(className)) {
+    currentElement = currentElement.parentNode;
   }
 
   return currentElement;
 }
 
 /**
- * 
- * @param { object } element 
- * @param { string } type 
+ *
+ * @param { object } element
+ * @param { string } type
  * @returns { object }
  */
-export function findancestorbytype( element, type ) {
-  let currentElement = element
-  const lowertype = type.toLowerCase()
-  while ( currentElement && currentElement.tagName && currentElement.tagName.toLowerCase() !== lowertype ) {
-      currentElement = currentElement.parentNode
+export function findancestorbytype(element, type) {
+  let currentElement = element;
+  const lowertype = type.toLowerCase();
+  while (
+    currentElement &&
+    currentElement.tagName &&
+    currentElement.tagName.toLowerCase() !== lowertype
+  ) {
+    currentElement = currentElement.parentNode;
   }
 
   return currentElement;
