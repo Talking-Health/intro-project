9dee9ae5ed3264acaa2123ad4b4bb92bd6e16ece
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-10-13 00:00:31 +0100

    Front end testing added

--- public/test/frontend.test.js
+++ public/test/frontend.test.js
@@ -1,30 +1,57 @@
-/* eslint-env mocha */
-
+// test/frontend.test.js
 const { JSDOM } = require("jsdom");
-const chai = require("chai");
-const expect = chai.expect;
-const frontend = require("../public/script.js"); // adjust path
+const { expect } = require("chai");
+
+// Suppose your code is in public/script.js which exports some functions
+const frontend = require("../public/script.js");  // adjust path
 
 describe("Frontend UI functions", () => {
   let dom, document;
 
   beforeEach(() => {
     dom = new JSDOM(`
+      <!DOCTYPE html>
       <html>
         <body>
           <table id="peopletable">
-            <tbody><tr><td>Kermit Frog</td><td></td></tr></tbody>
+            <thead>
+              <tr><th>Name</th><th colspan="7">Schedule</th><th>Action</th></tr>
+              <tr class="days">
+                <th></th><th>Mon</th><th>Tue</th><th>Wed</th><th>Thu</th><th>Fri</th><th>Sat</th><th>Sun</th><th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Kermit Frog</td>
+                <td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+                <td><button class="edit-btn">Edit</button></td>
+              </tr>
+            </tbody>
           </table>
         </body>
       </html>
-    `);
+    `, { runScripts: "dangerously", resources: "usable" });
     document = dom.window.document;
     global.document = document;
+    global.window = dom.window;
   });
 
-  it("should mark a day", () => {
-    const cell = document.querySelector("td:nth-child(2)");
-    cell.textContent = "X";
+  it("should mark a day when edit is called", () => {
+    // Example: assume your frontend has a function editPerson(name, dayIndex)
+    const result = frontend.editPerson("Kermit Frog", 2);
+    expect(result).to.be.true;
+
+    const cell = document.querySelector("#peopletable tbody tr td:nth-child(3)");
     expect(cell.textContent).to.equal("X");
+  });
+
+  // You can also simulate button click
+  it("clicking edit button triggers edit logic", () => {
+    const btn = document.querySelector(".edit-btn");
+    // pretend the button has an event listener attached in script.js
+    btn.click();
+    // Now assert table contents changed as expected
+    const cell = document.querySelector("#peopletable tbody tr td:nth-child(2)");
+    expect(cell.textContent).to.equal("X"); // or whatever the logic should do
   });
 });
